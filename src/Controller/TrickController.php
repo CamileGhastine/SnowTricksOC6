df5e826998ec20f57c3243a292336c83f47af5a7
camileGhastine <camileghastine@hotmail.com>
2020-03-24 17:14:12 +0100

    homePage : TrickController,Trick (entity), fixtures, index.html.twig

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrickController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(TrickRepository $repo)
+    {
+        $tricks = $repo->findAll();
+        return $this->render('trick/index.html.twig', [
+            'tricks' => $tricks,
+        ]);
+    }
+}
