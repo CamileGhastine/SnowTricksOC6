62650895e544b53d17e1192ce48a992dfa45873e
camileGhastine <camileghastine@hotmail.com>
2020-04-01 13:49:41 +0100

    groups : Entity Category

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -55,9 +55,15 @@ class Trick
      */
     private $comments;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category", mappedBy="tricks")
+     */
+    private $categories;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
+        $this->categories = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -139,6 +145,34 @@ class Trick
             if ($comment->getTrick() === $this) {
                 $comment->setTrick(null);
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->addTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->contains($category)) {
+            $this->categories->removeElement($category);
+            $category->removeTrick($this);
         }
 
         return $this;
