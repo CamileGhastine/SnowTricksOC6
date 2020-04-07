1837b154300f7204d73899bd0e5d120ea136843d
camileGhastine <camileghastine@hotmail.com>
2020-04-07 10:41:06 +0100

    UserFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+
+        $username = 'camile';
+        $password = $this->passwordEncoder->encodePassword($user, $username);
+
+        $user->setUsername($username)
+            ->setEmail($username.'@'.$username.'.fr')
+            ->setPassword($password)
+            ->setAvatar('images/users/camile.jpg')
+            ->setRegisteredAt(new \DateTime)
+        ;
+
+        $manager->persist($user);
+
 
         $manager->flush();
     }
