b40008420a4a600375254dd8f19cd27ad56e487d
camileGhastine <camileghastine@hotmail.com>
2020-04-19 14:25:10 +0000

    constraint and option ImageType

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -7,13 +7,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file', FileType::class)
+            ->add('file', FileType::class,[
+                'label' => false,
+                'required' => true,
+                'constraints' => new File([
+                'maxSize' => '1024k',
+                'mimeTypes' => [ 'image/jpeg', 'image/png'],
+                'mimeTypesMessage' => 'L\'image doit être au format jpeg ou png',
+            ])])
         ;
     }
 
