7a65e3c412840641a065a24056cb9f8debd17bf7
camileGhastine <camileghastine@hotmail.com>
2020-04-23 20:26:43 +0000

    regex video iframe

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\VideoRepository")
@@ -18,6 +19,10 @@ class Video
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(
+         *     pattern = "/^(<iframe\s).*src=.*(<\/iframe>)$/i",
+     *     message = "La valeur entrée n'est pas un format iframe valide"
+     * )
      */
     private $iframe;
 
