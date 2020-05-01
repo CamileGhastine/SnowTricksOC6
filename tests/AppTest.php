cfab85c58fe2b281f2819c3829cf8deb6f49b2d0
camileGhastine <camileghastine@hotmail.com>
2020-05-01 15:51:43 +0000

    first test phpunit

---
+++ tests/AppTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class AppTest extends TestCase
+{
+    public function testTests () {
+        $this->assertEquals(2, 1+1);
+    }
+
+}
