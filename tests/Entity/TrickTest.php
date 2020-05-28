5ff7ba0d081ce71195d5b17a0c8bf1c1d64e4dab
camileGhastine <camileghastine@hotmail.com>
2020-05-28 11:28:06 +0000

    test Trick class

--- tests/Entity/TrickTest.php
+++ tests/Entity/TrickTest.php
@@ -1,15 +1,53 @@
 <?php
 
-
 namespace App\Tests\Entity;
 
-
+use App\Entity\Category;
+use App\Entity\Trick;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TrickTest extends KernelTestCase
 {
-    public function testValidityEntity()
+    public function getTrick()
     {
-        $trick = new Trick();
+        $category = (new Category())
+            ->setTitle('grabs');
+        $user = new User();
+
+        return $trick = (new Trick($user))
+            ->setTitle('New title')
+            ->setDescription('New description')
+            ->addCategory($category);
+    }
+
+    public function assertHasErrors(Trick $trick, int $number = 0)
+    {
+        self::bootKernel();
+        $error = self::$container->get('validator')->validate($trick);
+
+        $this->assertCount($number, $error);
+    }
+
+    public function testValideTrickTitle()
+    {
+        $trick = $this->getTrick();
+
+        $this->assertHasErrors($trick);
+    }
+
+    public function testInvalideTrickTitle()
+    {
+        $trick = $this->getTrick();
+        $trick->setTitle('');
+        $this->assertHasErrors($trick, 2);
+
+        $trick->setTitle('N');
+        $this->assertHasErrors($trick, 1);
+    }
+
+    public function testUniqueTrickTitle()
+    {
+        $this->assertHasErrors($this->getTrick()->setTitle('New title'),1);
     }
 }
